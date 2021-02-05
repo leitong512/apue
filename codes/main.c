90dd49353f8a0b3daaa08ddaf485dededf70a00f
lleitong <leitong@xinyu668.com>
2021-02-05 12:40:49 +0800

    add file

--- codes/main.c
+++ codes/main.c
@@ -8,12 +8,14 @@
 #include "src/header.h"
 #include "src/7_process_enviroment/exit_atexit.h"
 #include "src/7_process_enviroment/malloc_realloc.h"
+#include "src/7_process_enviroment/getenv_setenv.h"
 
 int main(void)
 {
 
     //******第七章******//
     //test_exit_atexit();
-    test_malloc_realloc();
+    //test_malloc_realloc();
+    test_getenv_setenv();
     return 0;
 }
