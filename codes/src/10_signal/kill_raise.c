4e424b65f016fc025d7c53953b1cdfc47b26e14e
lleitong <leitong@xinyu668.com>
2021-03-02 11:41:25 +0800

    kill_raise

--- codes/src/10_signal/kill_raise.c
+++ codes/src/10_signal/kill_raise.c
@@ -15,6 +15,14 @@ extern void add_sigaction(int signo, sigset_t *set, int no_deffer, int restart,
 
 /*
  *  \brief sig_print ：信号处理函数
- *  \param signo :
- *
+ *  \param signo : 信号值
+ *  \param info : 指向 siginfo_t 的指针
+ *  \param context : 指向 context_t 的指针
  */
+
+static void sig_print(int signo, siginfo_t *info, void *context)
+{
+    printf("------ Begin signal Handler <signo=%s>---------\n\n", strsignal(signo));
+    psiginfo(info, "The siginfo is : \t");
+    printf("------ End signal Handler <signo=%s>---------\n\n", strsignal(signo));
+}
